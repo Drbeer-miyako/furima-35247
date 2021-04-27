dd7e5f268d3d9c2a6fb1e36b0ccd8a74e1adc670
Drbeer-miyako <80018151+Drbeer-miyako@users.noreply.github.com>
2021-04-27 17:40:33 +0900

    商品出品フォームの表示

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,24 @@
 class ItemsController < ApplicationController
   def index
+    @item = Item.new
   end
+
+  def new
+    @item = Item.new
+  end
+
+  def create
+    Item.create(item_params)
+  end
+
+  # def destroy
+  #   item = Item.find(params[:id])
+  #   item.destroy
+  # end
+
+  private
+  def item_params
+    params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_date_indication_id, :price, :)
+  end
+
 end
