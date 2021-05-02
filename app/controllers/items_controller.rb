9a10555a2d9b5066e2539f8f2edbdef98c77fd53
Drbeer-miyako <80018151+Drbeer-miyako@users.noreply.github.com>
2021-05-02 19:59:00 +0900

    first_commit

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,5 +1,6 @@
 class ItemsController < ApplicationController
   before_action :authenticate_user!, except: [:show, :index]
+  
   def index
     @item = Item.order("created_at DESC")
   end
@@ -21,14 +22,26 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  # def edit
-  #   @item = Item.find(params[:id])
-  # end
+  def edit
+    @item = Item.find(params[:id])
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(update_params)
+        redirect_to root_path
+    else
+        render :edit
+    end
+  end
 
   private
   def item_params
     params.require(:item).permit(:image, :title, :description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_date_indication_id, :price).merge(user_id: current_user.id)
   end
   
+  def update_params
+    params.require(:prototype).permit(:image, :title, :description, :category_id, :status_id, :delivery_charge_id, :prefecture_id, :delivery_date_indication_id, :price )
+end
 
 end
